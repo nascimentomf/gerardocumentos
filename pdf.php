a34e31bf0f54dced03b326040ca5b7bbc1f1c81e
Moises Nascimento <moises.fn@gmail.com>
2012-12-21 20:27:44 -0200

    Aprimorando algoritmo - expressao regular

--- pdf.php
+++ pdf.php
@@ -4,20 +4,20 @@
   $nome = $_POST['nome'];
   $score = $_POST['score'];
   
-  function pdf_replace($pattern, $replacement, $string){
-  	$len = strlen($pattern);
-  	$regex = '';
-  	
-  	for($i = 0; $i < $len; $i++){
-  		$regex .= $pattern[$i];
-  		if($i < $len - 1){
-  			$regex .= '(\)-?[0-9]+\( )?';
-  		}
-  		return ereg_replace($regex, $replacement, $string);
-  	}
+  function pdf_replace( $pattern, $replacement, $string )
+  {
+    $len = strlen( $pattern );
+    $regexp = '';
+    for ( $i = 0; $i<$len; $i++ )
+    {
+      $regexp .= $pattern[$i];
+      if ($i<$len-1)
+        $regexp .= "(\)\-{0,1}[0-9]*\(){0,1}";
+    }
+    return ereg_replace ( $regexp, $replacement, $string );
   }
   if(!$nome || !$score){
-  	echo '<h1>Erro: Essa página foi chamada de maneira incorreta.</h1>';
+  	echo '<h1>Erro: Essa página foi chamada de maneira incorreta Ops!.</h1>';
   }
   else{
   	//gera os cabecalhos para ajudar o navegador a escolher a aplicacao correta
@@ -27,23 +27,26 @@
   	$data = date('m-d-Y');
   	
   	//Abre Modelo
-  	$arquivo = 'util/PHPCertificacao.pdf';
-  	$saida = file_get_contents($arquivo);
+  	$arquivo = 'util/PHPCertificacao3.pdf';
+//   	$saida = file_get_contents($arquivo);
   	
-  	//Substitui os marcadores de lugar no modelo pelos nossos dados
-  	
-  	//$saida = pdf_replace('<<NAME>>',strtoupper($nome),$saida);
-  	//$saida = pdf_replace('<<Name>>',$nome,$saida);
-  	//$saida = pdf_replace('<<score>>',$score,$saida);
-  	//$saida = pdf_replace('<<mm/dd/yyyy>>',$data,$saida);
-  	
-  	//$saida = str_replace('<<NAME>>',strtoupper($nome),$saida);
-  	//$saida = str_replace('<<Name>>',$nome,$saida);
-  	//$saida = str_replace('<<score>>',$score,$saida);
-  	//$saida = str_replace('<<mm/dd/yyyy>>',$data,$saida);
-  	
-  	//Envia o documento gerado para o browser
-  	
-  	echo $saida;
+
+	if(!$fp = fopen ( $arquivo, 'r' )){
+         echo 'Ocorreu um erro';
+        }
+        else{
+	  //read our template into a variable
+	  $saida = fread( $fp, filesize( $arquivo ) );
+
+	  fclose ( $fp );
+
+	// replace the place holders in the template with our data
+	  $saida = str_replace( '<<NAME>>', strtoupper( $nome ), $saida );
+	  $saida = str_replace( '<<Name>>', $nome, $saida );
+	  $saida = str_replace( '<<score>>', $score, $saida );
+	  $saida = str_replace( '<<mm/dd/yyyy>>', $data, $saida );
+	  
+	  echo $saida;
+        }
   }
 ?>
